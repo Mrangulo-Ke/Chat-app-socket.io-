6c4a0e938c5f2e033102b34f8bed838f501d962e
Mwangekason <mwangekapatrick1@gmail.com>
2021-01-10 00:48:35 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+//const { Socket } = require('dgram')
+const Filter = require('bad-words')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+
+io.on('connection', (socket) => {
+	console.log('New WebSocket connection')
+
+	socket.emit('message', 'Welcome!')
+	socket.broadcast.emit('message', 'A new user has joined')
+
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter()
+
+		if(filter.isProfane(message)){
+			return callback('Profanity is not allowed!')
+		}
+
+		io.emit('message', message)
+		callback()
+	})
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'User has left!')
+	})
+
+	socket.on('sendLocation', (coords, callback) => {
+		io.emit('message', 'https://google.com/maps?=${coords.latitude},${coords.longitude}')
+		callback()
+	})
+})
+
+server.listen(port, () =>{
+	console.log('Server is up on port ' + port)
+})
