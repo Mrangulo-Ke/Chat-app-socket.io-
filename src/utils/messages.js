e239bbbaf6deedea845d6eee8744a1dadf2da122
Mwangekason <mwangekapatrick1@gmail.com>
2021-01-12 09:01:38 +0300

    timestamps for location messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -5,6 +5,15 @@ const generateMessage = (text) => {
     }
 }
 
+const generateLocationMessage = (url) => {
+    return {
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+
 module.exports = {
-    generateMessage
+    generateMessage,
+    generateLocationMessage
 }
