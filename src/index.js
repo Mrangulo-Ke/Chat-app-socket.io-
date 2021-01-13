a17f8f9bb7c8829e66bc3277847ba8f486e178ff
Mwangekason <mwangekapatrick1@gmail.com>
2021-01-13 11:39:19 +0300

    rendering user list

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 const { generateMessage,generateLocationMessage } = require('./utils/messages')
-const { addUser, removeUser, getUser, getUserInRoom } = require('./utils/users')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -29,6 +29,10 @@ io.on('connection', (socket) => {
 
 	    socket.emit('message', generateMessage('Admin', 'Welcome!'))
     	socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+    	io.to(user.room).emit('roomData', {
+    		room: user.room,
+    		users: getUsersInRoom(user.room)
+    	})
 
     	callback()
     })
@@ -56,6 +60,10 @@ io.on('connection', (socket) => {
 
     	if (user) {
     		io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+    		io.to(user.room).emit('roomData', {
+    			room: user.room,
+    			users: getUsersInRoom(user.room)
+    		})
     	}
     })
 })
